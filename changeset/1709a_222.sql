ccaa3f88133184f1132555f0d0805a33aaaf0855
Neil Pullar <neil.pullar@fao.org>
2017-09-21 06:44:55 +0700

    Fix bug in porting of simple transaction handling from CS Uganda by adding issued and historic (opentenure) claim status code list

---
+++ changeset/1709a_222.sql
@@ -0,0 +1,6 @@
+﻿SET search_path = opentenure, pg_catalog;
+--
+INSERT INTO claim_status VALUES ('issued', 'Issued', 'c', 'Final status for the claim, indicating it is issued to the owner') ON CONFLICT (code) DO NOTHING;
+INSERT INTO claim_status VALUES ('historic', 'Historic', 'c', 'Historic status, indicating that parcel was split or merged.') ON CONFLICT (code) DO NOTHING;
+--
+INSERT INTO system.version SELECT '1709a' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1708a');
